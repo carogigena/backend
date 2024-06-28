35440bd347bc595ee4ed043a41c4423001e12be6
carogigena <gigena.carola@gmail.com>
2024-06-28 12:46:31 -0300

    con DB 222

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(morgan("dev"));
 
 app.get("/", (req, res)=> {
-    res.send("VIVERO APP !!");
+    res.send("VIVERO APP 222 !!");
 
 });
 console.log("VIVERO backend!!");
