6985d7dc4b778459edf6a3e950baa5b08962da94
carogigena <gigena.carola@gmail.com>
2024-06-28 14:35:45 -0300

    7

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require("express");
-//const morgan = require('morgan');
+const morgan = require('morgan');
 const database = require("./db");
 
 const app = express();
 
 //Uso middleware
-//app.use(express.json());
-//app.use(morgan("dev"));
+app.use(express.json());
+app.use(morgan("dev"));
 
 app.get("/", (req, res)=> {
-    res.send("VIVERO APP 6 !!");
+    res.send("VIVERO APP 7 !!");
 
 });
 console.log("VIVERO backend!!");
