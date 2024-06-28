ed8fd480d7a1af17c5bfebf411d123f0fc430a29
carogigena <gigena.carola@gmail.com>
2024-06-28 12:34:03 -0300

    xxx

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res)=> {
-    res.send("VIVERO backend!!");
+    res.send("VIVERO back 2!!");
 
 });
 console.log("VIVERO backend!!");
