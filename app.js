69c897fddd2225f7326deacd500cd31dc513f34f
carogigena <gigena.carola@gmail.com>
2024-07-08 16:21:48 -0300

    updatecors

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const tipoproductosRouter = require('./routes/tipoproductos');
 const mensajesRouter = require('./routes/mensajes');
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({origin:'http://127.0.0.1:5501'}));
 app.use('/usuarios',usuariosRouter);
 app.use('/productos',productosRouter);
 app.use('/tipoproductos',tipoproductosRouter);
