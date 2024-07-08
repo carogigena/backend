0dbf600be04d877e440418b3578787ea3af65a3c
carogigena <gigena.carola@gmail.com>
2024-07-08 16:55:32 -0300

    cors

--- app.js
+++ app.js
@@ -12,14 +12,13 @@ const tipoproductosRouter = require('./routes/tipoproductos');
 const mensajesRouter = require('./routes/mensajes');
 
 app.use(express.json());
-app.use(cors({origin:'http://127.0.0.1:5501'}));
+app.use(
+    cors({
+      origin: "*",
+      methods: ["GET", "POST", "PUT", "DELETE"],
+    })
+  );
 
-app.use((req, res, next)=>{
-    res.append('Access-Control-Allow-Origin',['http://127.0.0.1:5501']);
-    res.append('Access-Control-Allow-Methods','GET, PUT, POST, DELETE');
-    res.append('Access-Control-Allow-Headers','Content-Type');
-    next();
-})
 app.use('/usuarios',usuariosRouter);
 app.use('/productos',productosRouter);
 app.use('/tipoproductos',tipoproductosRouter);
