e4541a5cfbd4ffe6d0b083bbe8ac1db21de4122d
carogigena <gigena.carola@gmail.com>
2024-07-10 01:45:15 -0300

    db

--- db/db.js
+++ db/db.js
@@ -8,7 +8,7 @@ const connection = mySql.createConnection(
         connectionLimit:10,
         connectTimeout : 10000,
         waitForConnections: true,
-        queueLimit: 0
+        queueLimit: 2
     });
 
 
