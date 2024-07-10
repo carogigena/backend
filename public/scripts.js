a8c4a62fc4583a3a67ad1272ec144957deac8b14
carogigena <gigena.carola@gmail.com>
2024-07-10 01:57:56 -0300

    del

--- public/scripts.js
+++ public/scripts.js
@@ -101,14 +101,7 @@ async function editProducto(id) {
 async function deleteProducto(id) {
     if (confirm('¿Estás seguro de que deseas eliminar este producto?')) {
          await fetch(`${apiUrl}/productos/${id}`, {
-            method: 'DELETE',
-            mode: 'cors',
-            headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'DELETE, HEAD, GET, OPTIONS, POST,PUT'
-            },
+            method: 'DELETE'
         })
         .then(() => listaProductos());
     }
